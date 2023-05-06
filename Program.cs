218cd71f0e987d84bbe55f6df8692b701791fb71
Arsalaann <arsalankhanxk@gmail.com>
2023-05-06 13:47:40 +0530

    Number of times dice rolled

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
 ﻿namespace SnakeAndLadder{
     class Program{
 
-        public static int player=0;
+        public static int player=0,diceRolled=0;
         public static void Main(string[] args){
             Program obj=new Program();
             Rolling roll=new Rolling();
             Choice choice=new Choice();
             Console.WriteLine("Player Position: "+Program.player+"\n---------------------");
-            while(Program.player<100)
+            while(Program.player<100){
                 choice.Decide(roll.Roll());
+                Program.diceRolled++;
+            }
+            Console.WriteLine("Total Dice Rolled: "+Program.diceRolled);
         }
     }
 }
